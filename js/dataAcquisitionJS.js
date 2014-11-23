33f69254cf5ad4ecaf29637b43554823e81e0e69
Akshat Thirani <akshatthirani@gmail.com>
2014-11-22 20:21:40 -0600

    Firebase functional

--- js/dataAcquisitionJS.js
+++ js/dataAcquisitionJS.js
@@ -1,15 +1,37 @@
+var boolBegin = 0;
+var f = 0;
+
 function buttonClicked(elem) {
 
 	elemID = elem.id;
 	  //  alert(elem.id);
-
-
 	currentvalue = document.getElementById(elemID).innerHTML;
   
   	if(currentvalue == "Off"){
-    document.getElementById(elemID).innerHTML="On";
+    	document.getElementById(elemID).innerHTML="On";
+    	f = f+1;	 
   	}else{
-    document.getElementById(elemID).innerHTML="Off";
+    	document.getElementById(elemID).innerHTML="Off";
+    	f = f-1;	
   	}
 
-}
+  	buttonClicked2();
+  	boolBegin = 1;
+};
+
+function buttonClicked2() {
+	var myFirebaseRef = new Firebase("https://amper.firebaseio.com/");
+
+
+	if(boolBegin == 0)
+		myFirebaseRef.set({
+		  "currentUsage": f,
+		});
+	else
+	{
+		myFirebaseRef.push({
+			"currentUsage": f
+		})
+	}
+
+};
